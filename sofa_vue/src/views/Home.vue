d427ef7b2aee23e19a1895218568a08bce54bae4
Dastan Sharchekev <47035775+Sleepycookie@users.noreply.github.com>
2021-09-16 14:06:41 +0200

    viewset in django

--- sofa_vue/src/views/Home.vue
+++ sofa_vue/src/views/Home.vue
@@ -3,10 +3,10 @@
     <section class="hero is-medium is-dark mb-6">
       <div class="hero-body has-text-centered">
         <p class="title mb-6">
-          Welcome to Djacket
+          Welcome to SOFA
         </p>
         <p class="subtitle">
-          The best jacket store online
+          The best men's jacket store online
         </p>
       </div>
     </section>
