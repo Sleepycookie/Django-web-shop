45ff842853afaf38b3cef377760deb30592ebab4
Dastan Sharchekev <47035775+Sleepycookie@users.noreply.github.com>
2021-09-17 14:20:03 +0200

    category details

--- sofa_django/product/serializers.py
+++ sofa_django/product/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 
 from .models import Category, Product
 
-
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
@@ -14,4 +13,16 @@ class ProductSerializer(serializers.ModelSerializer):
             "price",
             "get_image",
             "get_thumbnail"
+        )
+
+class CategorySerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True)
+
+    class Meta:
+        model = Category
+        fields = (
+            "id",
+            "name",
+            "get_absolute_url",
+            "products",
         )
