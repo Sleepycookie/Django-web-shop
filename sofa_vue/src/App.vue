0e3e5392e048d8a309e8b67536b89955ec301ad4
Dastan Sharchekev <47035775+Sleepycookie@users.noreply.github.com>
2021-09-17 14:44:30 +0200

    search bar and page is done

--- sofa_vue/src/App.vue
+++ sofa_vue/src/App.vue
@@ -2,44 +2,50 @@
   <div id="wrapper">
     <nav class="navbar is-dark">
       <div class="navbar-brand">
-        <router-link to="/" class="navbar-item"
-          ><strong>Sofa</strong></router-link
-        >
+        <router-link to="/" class="navbar-item"><strong>SOFA</strong></router-link>
 
-        <a
-          class="navbar-burger"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbar-menu"
-          @click="showMobileMenu = !showMobileMenu"
-        >
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
       </div>
 
-      <div
-        class="navbar-menu"
-        id="navbar-menu"
-        v-bind:class="{ 'is-active': showMobileMenu }"
-      >
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu }">
+        <div class="navbar-start">
+          <div class="navbar-item">
+            <form method="get" action="/search">
+              <div class="field has-addons">
+                <div class="control">
+                  <input type="text" class="input" placeholder="What are you looking for?" name="query">
+                </div>
+
+                <div class="control">
+                  <button class="button is-success">
+                      <span class="icon">
+                      <i class="fas fa-search"></i>
+                      </span>
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+
         <div class="navbar-end">
-          <router-link to="/leather" class="navbar-item"
-            >Leather jackets</router-link
-          >
-          <router-link to="/denim" class="navbar-item"
-            >Denim jackets</router-link
-          >
-          <router-link to="/winter" class="navbar-item"
-            >Winter jackets</router-link
-          >
+          <router-link to="/leather" class="navbar-item">Leather jackets</router-link>
+          <router-link to="/denim" class="navbar-item">Denim jackets</router-link>
+          <router-link to="/winter" class="navbar-item">Winter jackets</router-link>
 
           <div class="navbar-item">
             <div class="buttons">
-              <router-link to="/log-in" class="button is-light"
-                >Log in</router-link
-              >
+              <template v-if="$store.state.isAuthenticated">
+                <router-link to="/my-account" class="button is-light">My account</router-link>
+              </template>
+
+              <template v-else>
+                <router-link to="/log-in" class="button is-light">Log in</router-link>
+              </template>
 
               <router-link to="/cart" class="button is-success">
                 <span class="icon"><i class="fas fa-shopping-cart"></i></span>
@@ -56,7 +62,7 @@
     </div>
 
     <section class="section">
-      <router-view />
+      <router-view/>
     </section>
 
     <footer class="footer">
