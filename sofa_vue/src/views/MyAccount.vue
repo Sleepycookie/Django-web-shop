ff3ee368cd660a09000a317e0ff4404dbcfcb725
Dastan Sharchekev <47035775+Sleepycookie@users.noreply.github.com>
2021-09-23 16:45:53 +0200

    showing order component done

--- sofa_vue/src/views/MyAccount.vue
+++ sofa_vue/src/views/MyAccount.vue
@@ -26,8 +26,23 @@
 <script>
 import axios from 'axios'
 
+import OrderSummary from '@/components/OrderSummary.vue'
+
 export default {
     name: 'MyAccount',
+    components: {
+        OrderSummary
+    },
+    data() {
+        return {
+            orders: []
+        }
+    },
+    mounted() {
+        document.title = 'My account | SOFA'
+
+        this.getMyOrders()
+    },
     methods: {
         logout() {
             axios.defaults.headers.common["Authorization"] = ""
@@ -39,6 +54,20 @@ export default {
             this.$store.commit('removeToken')
 
             this.$router.push('/')
+        },
+        async getMyOrders() {
+            this.$store.commit('setIsLoading', true)
+
+            await axios
+                .get('/api/v1/orders/')
+                .then(response => {
+                    this.orders = response.data
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+
+            this.$store.commit('setIsLoading', false)
         }
     }
 }
